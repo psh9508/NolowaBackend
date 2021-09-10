a6dadf3458d7f6499a3e67fe7db0530046df444f
Masuri <psh0258@gmail.com>
2021-09-10 16:01:32 +0900

    Initial commit

---
+++ Nolowa/src/main/java/com/example/Nolowa/NolowaApplication.java
@@ -0,0 +1,13 @@
+package com.example.Nolowa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NolowaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NolowaApplication.class, args);
+	}
+
+}
---
+++ Nolowa/src/test/java/com/example/Nolowa/NolowaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Nolowa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NolowaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
