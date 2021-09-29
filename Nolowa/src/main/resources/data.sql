9aca315c0af344fdea8f406dcfdb07905fa9ade0
Masuri <psh0258@gmail.com>
2021-09-30 00:51:20 +0900

    🧹data.sql 순서 변경

--- Nolowa/src/main/resources/data.sql
+++ Nolowa/src/main/resources/data.sql
@@ -1,37 +1,18 @@
---insert into user values(1, 'psh0258@gmail.com', sysdate(), 'Asdf');
---insert into user values(2, 'Alice@gmail.com', sysdate(), 'a');
---insert into user values(3, 'Elena@gmail.com', sysdate(), 'a');
---insert into user values(4, 'Masuri@naver.com', sysdate(), 'a');
-insert into user values(5, 'aa', sysdate(), 'a');
+insert into profile_image values(9L, '844b2e56483db6f6f75611e891a84e9c38c727d02655f3d211a837f0aadfa31c', 'http://djsajinkwan.com/wp-content/uploads/2015/06/%E1%84%8C%E1%85%B3%E1%86%BC%E1%84%86%E1%85%A7%E1%86%BC%E1%84%89%E1%85%A2%E1%86%B7%E1%84%91%E1%85%B3%E1%86%AF1.jpg');
+insert into profile_image values(5L, 'hash1', 'https://file.mk.co.kr/meet/neds/2021/06/image_readtop_2021_530955_16225970114666915.jpg');
 
+insert into user values(1, 'psh0258@gmail.com', sysdate(), 'Asdf', null);
+insert into user values(2, 'Alice@gmail.com', sysdate(), 'a', null);
+insert into user values(3, 'Elena@gmail.com', sysdate(), 'a', null);
+insert into user values(4, 'Masuri@naver.com', sysdate(), 'a', null);
+insert into user values(5L, 'aa', sysdate(), 'a', 5L);
 
-insert into follower values(1L, 1L, 5L);
-insert into follower values(2L, 2L, 5L);
-insert into follower values(3L, 3L, 5L);
---insert into follower values(4L, 3L, 1L);
---insert into follower values(5L, 3L, 2L);
-
-
---insert into post values(1L, '안녕하세요 팬여러분들!! 방탄소년단의 팬앤스타 초이스 상 수상을 위해 여러분의 도움이 필요합니다.', '병아리감별사', sysdate(), '@_golden_gate_', 1L);
---insert into post values(2L, '[에릭]', '병아리감별사', sysdate(), '@_golden_gate_', 1L);
---insert into post values(3L, 'NCT 127 美 ''제임스 코든쇼'' 출연 화제! 신곡 ''Sticker'' 무대 최초 공개, 글로벌 팬열광!', '병아리감별사', sysdate(), '@_golden_gate_', 1L);
---insert into post values(4L, '테스트 스트링입니다.', 'xaxxxx', sysdate(), '@Majj_manuel', 2L);
---insert into post values(5L, 'We ard too far away with our 50K gap goal.', 'xaxxxx', sysdate(), '@Majj_manuel', 2L);
---insert into post values(6L, 'Stream의 요소들이 특정한 조건을 충족하는지 검사하고 싶은 경우에는 match 함수를 이용할 수 있다.', 'BTS_OFFICIAL', sysdate(), '@WE_T', 3L);
---insert into post values(7L, '예를 들어 요소들을 출력하기를 원할 때 다.', 'BTS_OFFICIAL', sysdate(), '@WE_T', 3L);
---insert into post values(8L, 'redicate를 받아 Boolean을 Key값으로 partitioning한다', 'BIGBANG_OFFICIAL', sysdate(), '@Anti', 4L);
+insert into post values(1L, '안녕하세요 팬여러분들!! 방탄소년단의 팬앤스타 초이스 상 수상을 위해 여러분의 도움이 필요합니다.', '병아리감별사', sysdate(), '@_golden_gate_', 1L);
+insert into post values(2L, '[에릭]', '병아리감별사', sysdate(), '@_golden_gate_', 1L);
+insert into post values(3L, 'NCT 127 美 ''제임스 코든쇼'' 출연 화제! 신곡 ''Sticker'' 무대 최초 공개, 글로벌 팬열광!', '병아리감별사', sysdate(), '@_golden_gate_', 1L);
+insert into post values(4L, '테스트 스트링입니다.', 'xaxxxx', sysdate(), '@Majj_manuel', 2L);
+insert into post values(5L, 'We ard too far away with our 50K gap goal.', 'xaxxxx', sysdate(), '@Majj_manuel', 2L);
+insert into post values(6L, 'Stream의 요소들이 특정한 조건을 충족하는지 검사하고 싶은 경우에는 match 함수를 이용할 수 있다.', 'BTS_OFFICIAL', sysdate(), '@WE_T', 3L);
+insert into post values(7L, '예를 들어 요소들을 출력하기를 원할 때 다.', 'BTS_OFFICIAL', sysdate(), '@WE_T', 3L);
+insert into post values(8L, 'redicate를 받아 Boolean을 Key값으로 partitioning한다', 'BIGBANG_OFFICIAL', sysdate(), '@Anti', 4L);
 insert into post values(9L, '하지만 만약 1개의 Stream으로부터 갯수, 합계, 평균, 최댓값, 최솟값을 한번에 얻고 싶은 경우에는 어떻게 할 수 있을까?', 'bbnannn99', sysdate(), '@banana', 5L);
---insert into post values(10L, 'Stream에서 작업한 결과를 1개의 String으로 이어붙이기를 원하는 경우에 Collectors.joining()을 이용할 수 있다', 'ninn211', sysdate(), '@best_programmer', 6L);
-
-
---insert into profile_Image values(1L, '844b2e56483db6f6f75611e891a84e9c38c727d02655f3d211a837f0aadfa31c', 'C:\\Users\\psh02\\OneDrive\\사진\\Nolowa\\ProfileImages\\1.jpg', 1L);
---insert into profile_Image values(2L, '844b2e56483db6f6f75611e891a84e9c38c727d02655f3d211a837f0aadfa31c', 'C:\\Users\\psh02\\OneDrive\\사진\\Nolowa\\ProfileImages\\1.jpg', 2L);
---insert into profile_Image values(3L, '844b2e56483db6f6f75611e891a84e9c38c727d02655f3d211a837f0aadfa31c', 'C:\\Users\\psh02\\OneDrive\\사진\\Nolowa\\ProfileImages\\1.jpg', 3L);
---insert into profile_Image values(4L, '844b2e56483db6f6f75611e891a84e9c38c727d02655f3d211a837f0aadfa31c', 'C:\\Users\\psh02\\OneDrive\\사진\\Nolowa\\ProfileImages\\1.jpg', 4L);
-
---insert into profile_image values(5L, '844b2e56483db6f6f75611e891a84e9c38c727d02655f3d211a837f0aadfa31c', 'C:\\Users\\psh02\\OneDrive\\사진\\Nolowa\\ProfileImages\\1.jpg');
-insert into profile_image values(5L, '844b2e56483db6f6f75611e891a84e9c38c727d02655f3d211a837f0aadfa31c', 'http://djsajinkwan.com/wp-content/uploads/2015/06/%E1%84%8C%E1%85%B3%E1%86%BC%E1%84%86%E1%85%A7%E1%86%BC%E1%84%89%E1%85%A2%E1%86%B7%E1%84%91%E1%85%B3%E1%86%AF1.jpg');
-
-
-
-
