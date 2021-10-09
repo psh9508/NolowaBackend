48b16433606d0b59c1e048da02f23839e0f0d258
Masuri <psh0258@gmail.com>
2021-10-10 01:01:11 +0900

    Auditing 이용해서 DB 삽입시간 자동으로 입력하게 변경

--- Nolowa/src/main/java/com/example/Nolowa/NolowaApplication.java
+++ Nolowa/src/main/java/com/example/Nolowa/NolowaApplication.java
@@ -4,11 +4,13 @@ import org.apache.tomcat.util.descriptor.LocalResolver;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 
 import java.util.Locale;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class NolowaApplication {
 
--- Nolowa/src/main/java/com/example/Nolowa/dataModels/Post.java
+++ Nolowa/src/main/java/com/example/Nolowa/dataModels/Post.java
@@ -3,12 +3,15 @@ package com.example.Nolowa.dataModels;
 import com.example.Nolowa.dataModels.Images.ProfileImage;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
 
 @Entity
 @Getter
+@EntityListeners(AuditingEntityListener.class)
 public class Post {
 
     @Id @GeneratedValue
@@ -21,6 +24,7 @@ public class Post {
 
     private String message;
 
+    @CreatedDate
     private LocalDateTime uploadedDateTime;
 }
 
