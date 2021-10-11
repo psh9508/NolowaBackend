9da339a35c7f390056664b0734360cfbb48ac36d
Masuri <psh0258@gmail.com>
2021-10-11 13:37:47 +0900

    Token 적용

--- Nolowa/src/main/java/com/example/Nolowa/authentication/AuthenticationController.java
+++ Nolowa/src/main/java/com/example/Nolowa/authentication/AuthenticationController.java
@@ -2,17 +2,25 @@ package com.example.Nolowa.authentication;
 
 import com.example.Nolowa.dataModels.User;
 import com.sun.istack.NotNull;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.context.HttpSessionSecurityContextRepository;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpSession;
 import java.util.Map;
 
 @RestController
 @RequestMapping("/Authentication")
 public class AuthenticationController {
 
+    private final AuthenticationManager authenticationManager;
     private final AuthenticationService service;
 
-    public AuthenticationController(AuthenticationService service) {
+    public AuthenticationController(AuthenticationManager authenticationManager, AuthenticationService service) {
+        this.authenticationManager = authenticationManager;
         this.service = service;
     }
 
@@ -22,10 +30,22 @@ public class AuthenticationController {
     }
 
     @PostMapping("/Login")
-    public User Login(@NotNull @RequestBody Map<String, String> param) throws Exception {
+    public User Login(@NotNull @RequestBody Map<String, String> param, HttpSession session) throws Exception {
         String id = param.get("id");
-        String Password = param.get("password");
+        String password = param.get("password");
 
-        return service.login(id, Password);
+        SetToken(id, password, session);
+
+        return service.login(id, password);
     }
+
+    private void SetToken(String id, String Password, HttpSession session) {
+        var token = new UsernamePasswordAuthenticationToken(id, Password);
+        var authentication = authenticationManager.authenticate(token);
+
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+        session.setAttribute(HttpSessionSecurityContextRepository.SPRING_SECURITY_CONTEXT_KEY, SecurityContextHolder.getContext());
+    }
+
+
 }
--- Nolowa/src/main/java/com/example/Nolowa/config/WebSecurityConfig.java
+++ Nolowa/src/main/java/com/example/Nolowa/config/WebSecurityConfig.java
@@ -3,10 +3,13 @@ package com.example.Nolowa.config;
 import com.example.Nolowa.account.AccountService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -26,19 +29,28 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests()
+        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.NEVER)
+                .and()
+                .authorizeRequests()
                 .antMatchers("/Authentication/Login"
                             , "/h2-console/**").permitAll()
+                .antMatchers(HttpMethod.OPTIONS, "/**").permitAll()
                 .anyRequest().authenticated()
                 .and()
                 .csrf().disable()
                 .headers().frameOptions().disable()
                 .and()
-                .httpBasic();
+                .httpBasic().disable();
     }
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.userDetailsService(accountService).passwordEncoder(NoOpPasswordEncoder.getInstance());
+    }
+
+    @Bean
+    @Override
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
     }
 }
