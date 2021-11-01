deef44af17488c876f0e12e8a5c9bab6f8db2bfb
Masuri <psh0258@gmail.com>
2021-11-02 00:15:09 +0900

    mockito를 이용한 테스트 코드 작성 중

--- Nolowa/src/test/java/com/example/Nolowa/controller/UserControllerTests.java
+++ Nolowa/src/test/java/com/example/Nolowa/controller/UserControllerTests.java
@@ -1,6 +1,7 @@
 package com.example.Nolowa.controller;
 
 import com.example.Nolowa.authentication.AuthenticationController;
+import com.example.Nolowa.authentication.AuthenticationService;
 import com.example.Nolowa.dataModels.Images.ProfileImage;
 import com.example.Nolowa.dataModels.Post;
 import com.example.Nolowa.dataModels.User;
@@ -10,17 +11,27 @@ import net.minidev.json.parser.ParseException;
 import org.json.JSONObject;
 import org.junit.Before;
 import org.junit.jupiter.api.*;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.web.JsonPath;
 import org.springframework.http.MediaType;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
@@ -39,17 +50,39 @@ public class UserControllerTests {
     @Autowired
     private AuthenticationController authenticationController;
 
+    @Mock
+    private AuthenticationService authenticationService;
+
     @Before
     public void setUp() {
-        var profileImage = new ProfileImage("http://testUrl/Endpoint.com", "testhashcode");
+        MockitoAnnotations.initMocks(this);
+    }
 
-        var testUser = User.builder()
-                .email("TestUser")
-                .password("TestPassword")
-                .profileImage(profileImage)
-                .build();
+    @Test
+    public void Mock을이용한테스트작성() {
+        var mockService = mock(AuthenticationService.class);
 
-        userRepository.save(testUser);
+        var controller = new AuthenticationController(mockService, null);
+
+        var mockUser = new User();
+        mockUser.setEmail("TestUser");
+        mockUser.setPassword("TestPassword");
+        mockUser.setProfileImage(new ProfileImage("http://mockUrl", "hash"));
+
+        when(mockService.login("TestUser", "TestPassword")).thenReturn(mockUser);
+
+        try {
+            Map<String, String> param = new HashMap<String, String>() {
+                {
+                    put("id", "TestUser");
+                    put("password", "TestPassword");
+                }
+            };
+
+            var loginedUser = controller.Login(param);
+        } catch (Exception ex) {
+
+        }
     }
 
     @Test
@@ -64,7 +97,9 @@ public class UserControllerTests {
                     .accept(MediaType.APPLICATION_JSON)
                     .contentType(MediaType.APPLICATION_JSON));
 
-            actions.andDo(print());
+            actions.andDo(print())
+                    .andExpect(status().isOk())
+                    .andExpect(jsonPath("profileImage").exists());
         } catch (Exception exception) {
         }
 
