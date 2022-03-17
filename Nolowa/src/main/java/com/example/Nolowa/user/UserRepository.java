acd8f7d984fe81ca49779a71371690b27731b1c3
Masuri <psh0258@gmail.com>
2022-03-17 23:37:07 +0900

    대소문자 구분없이 이름으로 유저 가져올 수 있는 함수 추가

--- Nolowa/src/main/java/com/example/Nolowa/user/UserRepository.java
+++ Nolowa/src/main/java/com/example/Nolowa/user/UserRepository.java
@@ -3,9 +3,11 @@ package com.example.Nolowa.user;
 import com.example.Nolowa.dataModels.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByEmailAndPassword(String email, String password);
     Optional<User> findByEmail(String email);
+    List<User> findAllByNameContainingIgnoreCase(String username);
 }
